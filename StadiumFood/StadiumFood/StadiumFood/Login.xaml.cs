ccd9082425965a3bd81f512975ae50308589adc3
Rafael Lopez <rafaellopez19941113@gmail.com>
2020-01-29 13:50:36 -0700

    Proyecto new

---
+++ StadiumFood/StadiumFood/StadiumFood/Login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using StadiumFood.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace StadiumFood
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Registar.Clicked += (sender, e) =>
+            {
+                Navigation.PushAsync(new Registrarse());
+            };
+            btnIS.Clicked += (sender, e) =>
+            {
+                ModelLogin L = new ModelLogin();
+                L.Nom_Usuario = Usuario.Text;
+                L.Contrasena = Contraseña.Text;
+                L.Ubicacion = "";
+
+                String Url = "http://192.168.43.45:8080/wsEstadiumFood/resources/estadiumfood/iniciosesion";
+
+                var http = (HttpWebRequest)WebRequest.Create(new Uri(Url));
+                http.Accept = "application/json";
+                http.ContentType = "application/json";
+                http.Method = "POST";
+
+                string parsedContent = JsonConvert.SerializeObject(L, Formatting.Indented);
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                Byte[] bytes = encoding.GetBytes(parsedContent);
+
+                Stream newStream = http.GetRequestStream();
+                newStream.Write(bytes, 0, bytes.Length);
+                newStream.Close();
+
+                var response = http.GetResponse();
+
+                var stream = response.GetResponseStream();
+                var sr = new StreamReader(stream);
+                var content = sr.ReadToEnd();
+            };
+        }        
+
+    }
+}
---
+++ StadiumFood/StadiumFood/StadiumFood/Registrarse.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StadiumFood
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registrarse : ContentPage
+    {
+        private string urlValidacion;
+
+        public Registrarse()
+        {
+            InitializeComponent();
+        }
+        private async void Registar_Click(object sender, EventArgs args)
+        {
+
+            ModelRegiter R = new ModelRegiter();
+            R.NomUsuario = User.Text;
+            R.NomCompleto = Nombre.Text;
+            R.CorreoElectronico = Correo.Text;
+            R.NumTelefono = Contacto.Text;
+            R.Sexo = "";
+            R.Contrasena = Contraseña.Text;
+            R.FecNacimieto = Fecha.Date;
+
+
+            String Url = "http://192.168.43.45:8080/wsEstadiumFood/resources/estadiumfood/registrarusuario";
+            
+            var http = (HttpWebRequest)WebRequest.Create(new Uri(Url));
+            http.Accept = "application/json";
+            http.ContentType = "application/json";
+            http.Method = "POST";
+
+            string parsedContent = JsonConvert.SerializeObject(R, Formatting.Indented);
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] bytes = encoding.GetBytes(parsedContent);
+
+            Stream newStream = http.GetRequestStream();
+            newStream.Write(bytes, 0, bytes.Length);
+            newStream.Close();
+
+            var response = http.GetResponse();
+
+            var stream = response.GetResponseStream();
+            var sr = new StreamReader(stream);
+            var content = sr.ReadToEnd();
+        }
+    }
+}
